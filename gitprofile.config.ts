a6aa3dfc24306e257efbfc2a2823d3f1af262c46
Umar Ulkhak <48248263+umarulkhak@users.noreply.github.com>
2024-08-09 22:00:01 +0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['umarulkhak/si-sdm', 'umarulkhak/edomphb-lstm'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['umarulkhak/si-sdm', 'umarulkhak/edomphb-lstm', 'umarulkhak/sambang-frontend', 'umarulkhak/sikisqu-app'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
