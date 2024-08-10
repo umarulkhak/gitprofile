02ba73a71f085bcb6a808b298940fbf5b4174b62
Umar Ulkhak <48248263+umarulkhak@users.noreply.github.com>
2024-08-11 03:25:54 +0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -189,7 +189,7 @@ const CONFIG = {
   blog: {
     source: 'medium', // medium | dev
     username: 'alkhak24', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
+    limit: 4, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
