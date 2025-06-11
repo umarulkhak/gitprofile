ef9b3be0ca300645ea65abbe4427768f162d014f
Umar Ulkhak <48248263+umarulkhak@users.noreply.github.com>
2025-06-11 12:37:04 +0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -7,9 +7,9 @@ const CONFIG = {
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
-   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
+   * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/''/gitprofile/'.
    */
-  base: '/gitprofile/',
+  base: '',
   projects: {
     github: {
       display: true, // Display GitHub projects?
